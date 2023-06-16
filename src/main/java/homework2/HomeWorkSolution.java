a7a79ceced64bfed5183fb2f8ad4416d83dc211c
Marina <112824047+javaschoolgirl@users.noreply.github.com>
2023-06-17 00:06:35 +0200

    Homework2

---
+++ src/main/java/homework2/HomeWorkSolution.java
@@ -0,0 +1,25 @@
+package homework2;
+
+public class HomeWorkSolution {
+    public static void main(String[] args) {
+        // 1 уровень сложности: Создайте переменные (любое имя) и определите типы (наиболее подходящие) для следующих выражений:
+        //
+        //(float) Math.pow(Math.E, 5) + 1.5;
+        //
+        //(byte) Math.log(2) + (short) 14;
+        //
+        //1 + 1.5;
+
+
+        double v = (float) Math.pow(Math.E, 5) + 1.5;
+        System.out.println(v);
+
+        int i = (byte) Math.log(2) + (short) 14;
+        System.out.println(i);
+
+        double v1 = 1 + 1.5;
+        System.out.println(v1);
+
+
+    }
+}
